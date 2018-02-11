e1abe03774a1b66627b33a362a311fcf716b9fba
Sharmodeep Sarkar <sharmodeepsarkar25@gmail.com>
2018-02-11 07:42:13 -0500

    CTC filter and shopping layout done

--- js/custom-shopJS.js
+++ js/custom-shopJS.js
@@ -3,13 +3,20 @@
  */
 
 function goSinglesPage( prodId){
-    var prodName = document.getElementById(prodId+"-name");
-    var prodPrice = document.getElementById(prodId+"-price");
+    var prodName = document.getElementById(prodId+"-name").innerText;
+    var prodPrice = document.getElementById(prodId+"-price").innerText;
     var prodDescription = "xx";
     console.log(prodName);console.log(prodId);console.log(prodDescription);console.log(prodPrice);
     localStorage.setItem("prodName", prodName);
     localStorage.setItem("prodId", prodId);
     localStorage.setItem("prodDescription", prodDescription);
     localStorage.setItem("prodPrice", prodPrice);
-    window.location.href = '/single.html';
+    window.location.href = 'single.html';
+}
+
+
+function teaTypeFilter(teaType) {
+    if (teaType === 'CTC'){
+        window.location.href = 'CTC.html';
+    }
 }
